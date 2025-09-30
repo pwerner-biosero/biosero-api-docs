b66268bc3676915825fbd5308aaeaf9b1af2c936
pwerner-biosero <166531015+pwerner-biosero@users.noreply.github.com>
2025-09-30 13:45:07 -0400

    feat: update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Overview
+            to="/docs/Python SDK/Query Client">
+            Get Started
           </Link>
         </div>
       </div>
